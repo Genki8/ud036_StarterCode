970ff128c8ea2361208a7d2f64bbfb4bbcef496d
Matthew Madaj <matthew_madaj@goodyear.com>
2018-02-08 12:30:46 -0500

    Added more comments and conformed to PEP-8

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,4 +1,5 @@
 """generates the content of the Movie trailers website"""
+
 import fresh_tomatoes
 import media
 
@@ -6,26 +7,27 @@ import media
 def main():
     """Initializes movie objects in an array and passes to fresh_tomatoes"""
 
+    # Matrix movie: title,duration, storyline, poster URL, and Trailer
     the_matrix = media.Movie("The Matrix", "1 hour",
                              "A computer hacker learns from mysterious rebels "
                              "about the true nature of his reality and his "
                              "role in the war against its controllers.",
                              "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
                              "https://www.youtube.com/watch?v=m8e-FF8MsqU")
-
+    # Interstellar movie: title,duration, storyline, poster URL, and Trailer
     interstellar = media.Movie("Interstellar", "1 hour",
                                "A team of explorers travel through a wormhole"
                                "in space in an attempt to ensure humanity's"
                                "survival.",
                                "https://images-na.ssl-images-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
                                "https://www.youtube.com/watch?v=2LqzF5WauAw")
-
+    # Dumb and Dumber movie: title,duration, storyline, poster URL, and Trailer
     dumb_and_dumber = media.Movie("Dumb and Dumber", "1 hour",
                                   "The cross-country adventures of two "
                                   "good-hearted but incredibly stupid friends",
                                   "https://images-na.ssl-images-amazon.com/images/M/MV5BZDQwMjNiMTQtY2UwYy00NjhiLTk0ZWEtZWM5ZWMzNGFjNTVkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR0,0,182,268_AL_.jpg",  # NOQA
                                   "https://www.youtube.com/watch?v=MSu25pQ4iFw")  # NOQA
-
+    # Infinity War movie: title,duration, storyline, poster URL, and Trailer
     rick_roll = media.Movie("Infinity War", "1 hour",
                             "The Avengers and their allies must be willing to"
                             " sacrifice all in an attempt to defeat the "
@@ -33,7 +35,7 @@ def main():
                             " and ruin puts an end to the universe.",
                             "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0MjA1OTMxOV5BMl5BanBnXkFtZTgwMzM1NDcyNDM@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
                             "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-
+    # Equalizer movie: title,duration, storyline, poster URL, and Trailer
     the_equalizer = media.Movie("The Equalizer", "1 hour",
                                 "A man believes he has put his mysterious"
                                 "past behind him and has dedicated himself to"
@@ -43,7 +45,7 @@ def main():
                                 "stand idly by - he has to help her.",
                                 "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ2MzE2NTk0NF5BMl5BanBnXkFtZTgwOTM3NTk1MjE@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
                                 "https://www.youtube.com/watch?v=VjctHUEmutw")
-
+    # Shawshank movie: title,duration, storyline, poster URL, and Trailer
     shawshank_redemption = media.Movie("Shawshank Redemption", "1 hour",
                                        "Two imprisoned men bond over a number"
                                        " of years, finding solace and eventua"
@@ -51,9 +53,11 @@ def main():
                                        "ecency.",
                                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg",  # NOQA
                                        "https://www.youtube.com/watch?v=6hB3S9bIaco")  # NOQA
-
+    # Place instances of Movie in an array
     movies = [rick_roll, dumb_and_dumber, interstellar,
               the_equalizer, shawshank_redemption, the_matrix]
+
+    # Generate and open the movies in a webbrowser using fresh_tomatoes.py
     fresh_tomatoes.open_movies_page(movies)
 
 if __name__ == '__main__':
--- media.py
+++ media.py
@@ -3,41 +3,51 @@
 import webbrowser
 
 
+# Define Video Class
 class Video(object):
     """ This class provides a way to store general video related information"""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     def __init__(self, title, duration):
+        """ Instantiates an object of type Video.
+            Input: Title, Duration """
         self.title = title
         self.duration = duration
 
 
+# Define TvShow Class
 class TvShow(Video):
     """ This class provides a way to store tv show related information """
 
     def __init__(self, show_title, duration, season, episode, tv_station):
+        """ Instantiates an object of type TvShow.
+            Input: Title, Duration, Season, Episode, TV Station """
         Video.__init__(self, show_title, duration)
         self.season = season
         self.episode = episode
         self.tv_station = tv_station
 
     def get_local_listing():
-        """functionality in development"""
+        """ functionality in development """
 
         print"something happens"
 
 
+# Define Movie Class
 class Movie(Video):
     """ This class provides a way to store movie related information """
 
     def __init__(self, movie_title, duration, movie_storyline, poster_image,
                  trailer_youtube):
+        """ Instantiates an object of type Movie.
+            Input: Title, Duration, Season, Story Line, Poster Image URL,
+            Trailer URL """
         Video.__init__(self, movie_title, duration)
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        """ shows the movie trailer for an object of type Movie"""
+        """ shows the movie trailer in browser for an object of type Movie """
 
         webbrowser.open(self.trailer_youtube_url)
